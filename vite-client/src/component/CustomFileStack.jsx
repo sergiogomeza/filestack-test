b60847a027579e87c9638cc91d9410d702cd725d
Sergio Gómez Arboleda <sergiogomez@drata.com>
2023-05-26 12:29:27 -0500

    add filestack picker configuration example

---
+++ vite-client/src/component/CustomFileStack.jsx
@@ -0,0 +1,37 @@
+import { useState } from "react";
+import { PickerOverlay } from "filestack-react";
+
+const CustomFilestack = () => {
+  const YOUR_API = import.meta.env.VITE_API_KEY;
+  const [isPickerVisible, setIsPickerVisible] = useState(false);
+
+  const pickerOptions = {
+    onClose,
+  };
+
+  function onClose() {
+    setIsPickerVisible(false);
+  }
+
+  function showPicker() {
+    setIsPickerVisible(true);
+  }
+
+  return (
+    <div>
+      {isPickerVisible && (
+        <PickerOverlay
+          apikey={YOUR_API}
+          pickerOptions={pickerOptions}
+          ß
+          onError={(error) => console.log("error: ", error)}
+          onUploadDone={(res) => console.log("upload: ", res)}
+          onClick={(res) => console.log("click: ", res)}
+        />
+      )}
+      <button onClick={showPicker}>Show Picker</button>
+    </div>
+  );
+};
+
+export default CustomFilestack;
